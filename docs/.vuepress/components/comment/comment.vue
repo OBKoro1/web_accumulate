7ab0e534eabf185f0e31344afbf4518e9d2607cd
OBKoro1 <1677593011@qq.com>
2019-06-24 19:15:32 +0800

    gitalk调试

--- docs/.vuepress/components/comment/comment.vue
+++ docs/.vuepress/components/comment/comment.vue
@@ -3,7 +3,7 @@
  * @Author: OBKoro1
  * @Created_time: 2019-06-23 14:48:30
  * @LastEditors: OBKoro1
- * @LastEditTime: 2019-06-24 19:11:40
+ * @LastEditTime: 2019-06-24 19:14:57
  * @Description: gitalk评论组件
  * 文章：https://juejin.im/post/5c9e30fb6fb9a05e1c4cecf6
  -->
@@ -99,7 +99,7 @@ export default {
           id: articleTile, // issue title
           title,
           body,
-          labels: ['documentdocumentdocumentdocumentdocumentdocumentdocument'],
+          labels: ['documentdocumentdocumentdocumentdocumentd', 'documentdocumentdocumentdocumentdocumentd', '啦啦啦啦啦啦啦'],
           // labels: labels,
           distractionFreeMode: false
         };
