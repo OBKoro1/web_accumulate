1e9a216ff778e8d34b410f3cb241775229723976
OBKoro1 <1677593011@qq.com>
2019-06-26 18:41:29 +0800

    gitalk调试

--- docs/.vuepress/components/comment/comment.vue
+++ docs/.vuepress/components/comment/comment.vue
@@ -3,7 +3,7 @@
  * @Author: OBKoro1
  * @Created_time: 2019-06-23 14:48:30
  * @LastEditors: OBKoro1
- * @LastEditTime: 2019-06-26 16:01:25
+ * @LastEditTime: 2019-06-26 18:40:31
  * @Description: gitalk评论组件
  * 文章：https://juejin.im/post/5c9e30fb6fb9a05e1c4cecf6
  -->
