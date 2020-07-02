60f8e31583d3b578e3ec9ab901ebe3c428e84cb7
OBKoro1 <1677593011@qq.com>
2020-07-02 16:27:35 +0800

    init

--- docs/.vuepress/config/algorithm.js
+++ docs/.vuepress/config/algorithm.js
@@ -1,53 +1,83 @@
 /*
  * Author: OBKoro1
  * Date: 2019-10-22 17:07:54
- * LastEditors: OBKoro1
- * LastEditTime: 2019-10-31 20:00:57
- * FilePath: /web_accumulate/docs/.vuepress/config/algorithm.js
+ * LastEditors  : OBKoro1
+ * LastEditTime : 2020-07-02 16:21:26
+ * FilePath     : \web_accumulate\docs\.vuepress\config\algorithm.js
  * Description: 前端算法页面配置
  */
 
 const simple = [
-    '/algorithm/induction/数组重复次数',
-    '/algorithm/induction/水仙花数',
-    '/algorithm/induction/反转3位整数',
-    '/algorithm/induction/查找斐波纳契数列中第N个数',
-    '/algorithm/simple/回文',
-    '/algorithm/simple/反转整数',
-    '/algorithm/simple/姓名去重',
-    '/algorithm/simple/分解质因数',
-    '/algorithm/simple/合并排序数组',
-    '/algorithm/simple/搜索二维矩阵',
-    '/algorithm/simple/字符串密钥格式',
-    '/algorithm/simple/最大子数组',
-    '/algorithm/simple/比较字符串',
-    '/algorithm/simple/两数之和',
-    '/algorithm/simple/中位数',
-    '/algorithm/simple/落单的数',
-    '/algorithm/simple/爬楼梯',
-    '/algorithm/simple/最长单词',
-    '/algorithm/simple/子数组之和',
-    '/algorithm/simple/检测2的幂次',
-    '/algorithm/simple/两个字符串是变位词',
-    '/algorithm/simple/删除元素',
-    '/algorithm/simple/第一个只出现一次的字符',
-    '/algorithm/simple/字符串压缩',
-    '/algorithm/simple/判断字符串的循环移动',
-    '/algorithm/simple/丢失的数',
-    '/algorithm/simple/相亲数',
-    '/algorithm/simple/爬楼梯2',
-    '/algorithm/simple/奇偶分割数组'
-]
+  "/algorithm/induction/数组重复次数",
+  "/algorithm/induction/水仙花数",
+  "/algorithm/induction/反转3位整数",
+  "/algorithm/induction/查找斐波纳契数列中第N个数",
+  "/algorithm/simple/回文",
+  "/algorithm/simple/反转整数",
+  "/algorithm/simple/姓名去重",
+  "/algorithm/simple/分解质因数",
+  "/algorithm/simple/合并排序数组",
+  "/algorithm/simple/搜索二维矩阵",
+  "/algorithm/simple/字符串密钥格式",
+  "/algorithm/simple/最大子数组",
+  "/algorithm/simple/比较字符串",
+  "/algorithm/simple/两数之和",
+  "/algorithm/simple/中位数",
+  "/algorithm/simple/落单的数",
+  "/algorithm/simple/爬楼梯",
+  "/algorithm/simple/最长单词",
+  "/algorithm/simple/子数组之和",
+  "/algorithm/simple/检测2的幂次",
+  "/algorithm/simple/两个字符串是变位词",
+  "/algorithm/simple/删除元素",
+  "/algorithm/simple/第一个只出现一次的字符",
+  "/algorithm/simple/字符串压缩",
+  "/algorithm/simple/判断字符串的循环移动",
+  "/algorithm/simple/丢失的数",
+  "/algorithm/simple/相亲数",
+  "/algorithm/simple/爬楼梯2",
+  "/algorithm/simple/奇偶分割数组",
+];
 
 const medium = [
-    '/algorithm/medium/第k大元素',
-    '/algorithm/medium/丑数',
-    '/algorithm/medium/统计数字',
-    '/algorithm/medium/无重复字符的最长子串',
-    '/algorithm/medium/摆动序列'
-]
+  "/algorithm/medium/5最长回文子串",
+  "/algorithm/medium/15三数之和",
+  "/algorithm/medium/146LRU缓存机制",
+  "/algorithm/medium/11题盛最多水的容器",
+  "/algorithm/medium/33搜索旋转排序数组",
+  "/algorithm/medium/56合并区间",
+  "/algorithm/medium/46全排列",
+  "/algorithm/medium/199二叉树的右视图",
+  "/algorithm/medium/102二叉树的层序遍历",
+  "/algorithm/medium/200题岛屿数量",
+  "/algorithm/medium/93复原IP地址",
+  "/algorithm/medium/54螺旋矩阵",
+  "/algorithm/medium/103二叉树的锯齿形层次遍历",
+  "/algorithm/medium/322零钱兑换",
+  "/algorithm/medium/19删除链表的倒数第N个节点",
+  "/algorithm/medium/22括号生成",
+  "/algorithm/medium/351安卓系统手势解锁",
+  "/algorithm/medium/22括号生成",
+
+  
+
+  "/algorithm/medium/55题跳远游戏",
+  "/algorithm/medium/64题最小路径和",
+  "/algorithm/medium/456题132模式",
+  "/algorithm/medium/466统计重复个数",
+  "/algorithm/medium/735题行星碰撞",
+  "/algorithm/medium/1229安排会议日程",
+  "/algorithm/medium/45题跳跃游戏2",
+  "/algorithm/medium/867题转置矩阵",
+  "/algorithm/medium/奇数未小于相邻偶数为的值",
+  "/algorithm/medium/第k大元素",
+  "/algorithm/medium/丑数",
+  "/algorithm/medium/统计数字",
+  "/algorithm/medium/无重复字符的最长子串",
+  "/algorithm/medium/摆动序列",
+];
 
 module.exports = {
-    simple,
-    medium
-}
+  simple,
+  medium,
+};
